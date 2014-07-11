295fb592533c35529f75f69435182967fa4d79d6
Jose Luis Cordero <jocorbre@gmail.com>
2014-07-11 10:17:01 -0600

    this commit does not matter

--- Frontend/app/js/controllers.js
+++ Frontend/app/js/controllers.js
@@ -8,22 +8,46 @@ app= angular.module('myApp.controllers', [])
 
   }])
   
-  .controller('MyCtrl2', ['$scope', function($scope, $http) {
+  .controller('MyCtrl2', ['$scope', '$http','$templateCache', function($scope, $http, $templateCache) {
 	$scope.dataValue=2;		
 	
+	$scope.method = 'GET';
+    $scope.url = 'http://rest-service.guides.spring.io/greeting';
 	/*
-	$http.get('http://localhost:3000/user')
+	$http.get('http://rest-service.guides.spring.io/greeting')
 		.success(function(data) {
 			$scope.userList = data;
 			console.log(data);
-		})
-		.error(function(data) {
-			console.log('Error: ' + data);
 		});
-		*/
+	alert(data);
+	*/
+	
+	var userList1= [{id:'hola'}];
+	
+	$http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+        success(function(data, status) {
+          $scope.userList = data;	
+			alert(data);	  
+        }).
+        error(function(data, status) {
+         
+      });
+	
+	
 	
 	$scope.driversList = [
       {
+          Driver: {
+              givenName: 'Sebastian',
+              familyName: 'Vettel'
+          },
+          points: 322,
+          nationality: "German",
+          Constructors: [
+              {name: "Red Bull"}
+          ]
+      },
+	  {
           Driver: {
               givenName: 'Sebastian',
               familyName: 'Vettel'
